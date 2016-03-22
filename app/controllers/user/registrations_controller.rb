fbc7327df329dd14ecd6a5a7b26c3886c6bc28e2
jagvir1902 <jagvir1902@gmail.com>
2016-03-22 11:41:27 +0000

    Update views and navigation

--- app/controllers/user/registrations_controller.rb
+++ app/controllers/user/registrations_controller.rb
@@ -1,11 +1,11 @@
-class User::RegistrationController < Devise::RegistrationsController
+class User::RegistrationsController < Devise::RegistrationsController
 
   before_filter :configure_permitted_parameters
   
   protected
   
   def configure_permitted_parameters
-    devide_parameter_sanitizer.for(sign_up).push(:first_name, :last_name)
+    devise_parameter_sanitizer.for(:sign_up).push(:first_name, :last_name)
     devise_parameter_sanitizer.for(:account_update).push(:first_name, :last_name)
   end
   
