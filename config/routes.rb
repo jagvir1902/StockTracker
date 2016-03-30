4d2544eb062c7b7d3917f19ecd542cc2b23cc437
jagvir1902 <jagvir1902@gmail.com>
2016-03-30 15:32:33 +0000

    Add view for search friends

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,8 @@ root "welcome#index"
 get 'my_portfolio', to:'users#my_portfolio'
 get 'search_stocks', to:'stocks#search'
 get 'my_friends', to: 'users#my_friends'
-
+get 'search_friends', to: 'users#search'
+post 'add_friend', to: 'users#add_friends'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
